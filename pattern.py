575d7decb5b950230d797a9bc18dbfad666a9cc6
Rani patil <ranisamratpatil@gmail.com>
2022-07-11 22:07:18 -0400

    pattern change

--- pattern.py
+++ pattern.py
@@ -1,7 +1,8 @@
-a=65
-for i in range(65,70):
-    for j in range(65,i+1):
-       # print(end="\n")
-        print(chr(j), end=" ")
+ #n=int(input("Enter any number"))
+
+for i in range(0,6):
+    print(end="\n")
+    for j in range(65,65+i):
+      print(chr(j), end=" ")
             #a= a+1 
             #print()
